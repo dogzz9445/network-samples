6960ac6394aced5c8bed8f8bb2ee26f0d1608db0
Dongmin Jang (장동민) <37837785+dogzz9445@users.noreply.github.com>
2022-04-09 23:04:36 +0900

    Add wpf ui at wpf_tcp_server

---
+++ samples/wpf_tcp_server/View/HomePage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Controls;
+using wpf_tcp_server.ViewModel;
+
+namespace wpf_tcp_server.View
+{
+    /// <summary>
+    /// HomePage.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class HomePage : Page
+    {
+        private HomeViewModel _viewModel;
+        
+        public HomePage()
+        {
+            InitializeComponent();
+
+            _viewModel = new HomeViewModel();
+            DataContext = _viewModel;
+        }
+    }
+}
---
+++ samples/wpf_tcp_server/ViewModel/HomeViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace wpf_tcp_server.ViewModel
+{
+    public class HomeViewModel : BindableBase
+    {
+        private bool _isListening;
+        public bool IsListening { get => _isListening; set => SetProperty(ref _isListening, value); }
+
+        private readonly ObservableCollection<string> _logs = new ObservableCollection<string>();
+
+        public HomeViewModel()
+        {
+            IsListening = false;
+
+        }
+
+        public void AddLog(string log)
+        {
+            _logs.Add(log);
+        }
+
+    }
+}
