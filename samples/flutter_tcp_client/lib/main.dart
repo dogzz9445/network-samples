79ec731bedae160c46a25d88bbf3310f7d8eb3f4
Dongmin Jang (장동민) <37837785+dogzz9445@users.noreply.github.com>
2022-04-15 22:31:44 +0900

    Commit before split tcp_clinet of main.dart at flutter_tcp_client

--- samples/flutter_tcp_client/lib/main.dart
+++ samples/flutter_tcp_client/lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'dart:io';
 import 'dart:typed_data';
 
@@ -11,21 +10,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'TCP 클라이언트',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'TCP 클라이언트'),
@@ -35,15 +24,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -106,36 +86,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
           child: Column(
-        // Column is also a layout widget. It takes a list of children and
-        // arranges them vertically. By default, it sizes itself to fit its
-        // children horizontally, and tries to be as tall as its parent.
-        //
-        // Invoke "debug painting" (press "p" in the console, choose the
-        // "Toggle Debug Paint" action from the Flutter Inspector in Android
-        // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-        // to see the wireframe for each widget.
-        //
-        // Column has various properties to control how it sizes itself and
-        // how it positions its children. Here we use mainAxisAlignment to
-        // center the children vertically; the main axis here is the vertical
-        // axis because Columns are vertical (the cross axis would be
-        // horizontal).
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
@@ -178,39 +134,63 @@ class _MyHomePageState extends State<MyHomePage> {
           Card(
             child: Column(children: [
               const Text("전송 테스트"),
-              ElevatedButton(
-                  onPressed: () {
-                    _send("hello");
-                  },
-                  child: const Text("전송"))
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        _send("hello");
+                      },
+                      child: const Text("주기적으로 전송")),
+                  ElevatedButton(
+                      onPressed: () {
+                        _send("hello");
+                      },
+                      child: const Text("전송")),
+                  ElevatedButton(
+                      onPressed: () {
+                        _send("hello");
+                      },
+                      child: const Text("전송")),
+                  ElevatedButton(
+                      onPressed: () {
+                        _send("hello");
+                      },
+                      child: const Text("전송"))
+                ],
+              )
             ]),
           ),
           Card(
+              margin: EdgeInsets.all(10),
               child: Column(
-            children: [
-              const Text("로그"),
-              ListView.builder(
-                shrinkWrap: true,
-                itemBuilder: (context, index) {
-                  return Text(_logs.value[index]);
-                },
-                itemCount: _logs.value.length,
-              ),
-            ],
-          ))
+                children: [
+                  const Text("로그"),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      return Text(_logs.value[index]);
+                    },
+                    itemCount: _logs.value.length,
+                  ),
+                ],
+              ))
         ],
       )),
-      // TODO: 토글로 바꾸기
       floatingActionButton: _isConnected
           ? FloatingActionButton(
               onPressed: _disconnect,
               tooltip: 'Disconnect',
-              child: const Icon(Icons.cancel),
+              child: const Icon(
+                Icons.cancel,
+                semanticLabel: "연결해제",
+              ),
             )
           : FloatingActionButton(
               onPressed: _connect,
               tooltip: 'Connect',
-              child: const Icon(Icons.bolt),
+              child: const Icon(Icons.bolt, semanticLabel: "연결"),
             ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
---
+++ samples/flutter_tcp_client/lib/pages/tcp_client_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TcpClientPage extends StatefulWidget {
+  const TcpClientPage({Key? key}) : super(key: key);
+
+  @override
+  _TcpClientPageState createState() => _TcpClientPageState();
+}
+
+class _TcpClientPageState extends State<TcpClientPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
